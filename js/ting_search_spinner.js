6b97ea11e53b6fe5e86f8df492d2a1c35ac22f45
Giannis Chionidis <gc@inlead.dk>
2013-04-29 10:42:08 +0200

    case 1718 - Adding EOL plus code indentation. Adding scroll locking.

--- js/ting_search_spinner.js
+++ js/ting_search_spinner.js
@@ -1,11 +1,11 @@
 (function($) {
-  Drupal.behaviors.tingSearchAutocomplete = {
+  Drupal.behaviors.tingSearchSpinner = {
     attach: function(context) {
       
-      $('body').append('<div class="ting-search-spinner-placeholder-overlay"><div class="ting-search-spinner-placeholder-wrapper"><img src="'+Drupal.settings.spinner.path+'/img/spinner_squares_circle.gif" alt="Loading" /><p class="close-ting-search-spinner">Stop search</p></div></div>');
+      $('body').append('<div class="ting-search-spinner-placeholder-overlay"><div class="ting-search-spinner-placeholder-wrapper"><img src="' + Drupal.settings.spinner.path + '/img/spinner_squares_circle.gif" alt="Loading" /><p class="close-ting-search-spinner">' + Drupal.settings.spinner.stop_string + '</p></div></div>');
       $('.ting-search-spinner-placeholder-overlay').hide();
-      $('#search-block-form').submit(function(event, killPageLoad) {
-        if(killPageLoad==true){
+      $('#search-block-form').submit( function(event, killPageLoad) {
+        if (killPageLoad==true) {
           if (navigator.appName == 'Microsoft Internet Explorer') {
             document.execCommand('Stop');
           } 
@@ -15,12 +15,53 @@
           return false;
         }
         $('.ting-search-spinner-placeholder-overlay').show();
+        disable_scroll();
         return true;
       });  
+
+      // left: 37, up: 38, right: 39, down: 40,
+      // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+      var keys = [37, 38, 39, 40];
+
+      function preventDefault(e) {
+        e = e || window.event;
+        if (e.preventDefault)
+          e.preventDefault();
+        e.returnValue = false;  
+      }
+
+      function keydown(e) {
+        for (var i = keys.length; i--;) {
+          if (e.keyCode === keys[i]) {
+            preventDefault(e);
+            return;
+          }
+        }
+      }
+
+      function wheel(e) {
+        preventDefault(e);
+      }
+      
+      function disable_scroll() {
+        if (window.addEventListener) {
+          window.addEventListener('DOMMouseScroll', wheel, false);
+        }
+        window.onmousewheel = document.onmousewheel = wheel;
+        document.onkeydown = keydown;
+      }
+
+      function enable_scroll() {
+        if (window.removeEventListener) {
+          window.removeEventListener('DOMMouseScroll', wheel, false);
+        }
+        window.onmousewheel = document.onmousewheel = document.onkeydown = null;  
+      }
 
       // Cancel search when text is clicked.
       $('.close-ting-search-spinner').click(function () {
         $('.ting-search-spinner-placeholder-overlay').hide();
+        enable_scroll();
         $('#search-block-form').trigger('submit', [true]);
       });
       
@@ -28,9 +69,10 @@
       $(document).keyup(function(e) {
         if (e.keyCode == 27) { 
           $('.ting-search-spinner-placeholder-overlay').hide();
+          enable_scroll();
           $('#search-block-form').trigger('submit', [true]);  
         }  
       });
     }
   }
- } ($));
+})(jQuery);
